19b0515645b6797530557a1cbcfd8228b405cbed
zariahakuful843-cloud <zariahakuful843@gmail.com>
2026-05-27 00:07:05 +0100

    Update seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { PrismaClient } from "../src/generated/prisma";
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import bcrypt from "bcryptjs";
 
